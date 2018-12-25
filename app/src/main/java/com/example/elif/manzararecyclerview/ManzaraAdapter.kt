2fa8ac63e3a5f04ed7b5efc1a68407615b63a3b4
elifsezer <ellfsezer60@gmail.com>
2018-12-25 14:50:32 +0300

    Swipe create

--- app/src/main/java/com/example/elif/manzararecyclerview/ManzaraAdapter.kt
+++ app/src/main/java/com/example/elif/manzararecyclerview/ManzaraAdapter.kt
@@ -1,20 +1,29 @@
 package com.example.elif.manzararecyclerview
 
+import android.media.Image
 import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.tek_satir_manzara.view.*
+import org.w3c.dom.Text
 
 //primary constractır ilk tetiklenen yerdir. O yüzden buraya manzaralar tipinde bir arraylist beklediğimizi ifade ettik.
 class ManzaraAdapter(tumManzaralar: ArrayList<Manzara>) : RecyclerView.Adapter<ManzaraAdapter.ManzaraViewHolder>() {
     var manzaralar = tumManzaralar
-
     override fun getItemCount(): Int {
         Log.e("RecyclerView", "GetItemCount Tetiklendi.")
         return manzaralar.size
+    }
+
+    fun removeItem(position: Int) {
+        manzaralar.removeAt(position)
+        notifyItemRemoved(position)
+        notifyItemRangeChanged(position,manzaralar.size)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ManzaraViewHolder {
@@ -23,58 +32,38 @@ class ManzaraAdapter(tumManzaralar: ArrayList<Manzara>) : RecyclerView.Adapter<M
         var tekSatirmanzara = inflater.inflate(R.layout.tek_satir_manzara, parent, false)
         Log.e("RecyclerView", "OnCreateViewHolder Tetiklendi.")
         return ManzaraViewHolder(tekSatirmanzara)
-
     }
 
     //ayrıştırılan elemanları burada atama işlemi yaptık.
     override fun onBindViewHolder(holder: ManzaraViewHolder, position: Int) {
 
-        var oanOlusturulanManzara = manzaralar.get(position)
-        holder.setData(oanOlusturulanManzara, position)
+        holder.manzaraBaslik.text = manzaralar.get(position).baslik
+        holder.manzaraAciklama.text = manzaralar.get(position).aciklama
 
-        /* holder.manzaraBaslik.text = manzaralar.get(position).baslik
-         holder.manzaraAciklama.text = manzaralar.get(position).aciklama
-         holder.manzaraResim.setImageResource(manzaralar.get(position).resim)
-         Log.e("RecyclerView", "OnBindViewHolder Tetiklendi.")*/
+        Log.e("RecyclerView", "OnBindViewHolder Tetiklendi.")
     }
 
     //elemanları bulma işlemi erişme kısmı burasıdır.
     //inflate edilen alan buraya geldi Dnüştüülen xmlde hangi alanlar var ise burada değişkene çıkardık.
-  inner  class ManzaraViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ManzaraViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         //itemView:View= View genel bir sınıf o yüzden okuyamadık o yüzden typecasting yapıldı.
         var teksatirManzara = itemView as CardView
         var manzaraBaslik = teksatirManzara.tvManzaraBaslik
         var manzaraAciklama = teksatirManzara.tvManzaraAciklama
-        var manzaraResim = teksatirManzara.imgManzara
-        var btnKopyala = teksatirManzara.imgKopyala
-        var btnSil = teksatirManzara.imgSil
 
         //log kaydını yazamabilmem için bir methodun içinde olması gerekir. O yüzden inite koyduk
         init {
-            Log.e("RecyclerView", "ManzaraViewHolder Tetiklendi.")
-        }
+            manzaraBaslik.tvManzaraBaslik as TextView
+            manzaraAciklama.tvManzaraAciklama as TextView
 
-        fun setData(oankiManzara: Manzara, position: Int) {
-            //oankiManzara:Manzara tipinde direk Manzara sınıfımızdaki alanlara eriştik
-            manzaraBaslik.text = oankiManzara.baslik
-            manzaraAciklama.text = oankiManzara.aciklama
-            manzaraResim.setImageResource(oankiManzara.resim)
-            btnKopyala.setOnClickListener {
-
-                manzaralar.add(position,oankiManzara)
-                notifyItemInserted(position)
-                notifyItemRangeChanged(position,manzaralar.size)
-
-
-            }
-            btnSil.setOnClickListener {
-                Log.e("Elif","Silinmeden önce item pos"+ "Liste dize: "+manzaralar.size)
-                manzaralar.removeAt(position)
-                Log.e("Elif","Silinen item pos"+ "Liste dize: "+manzaralar.size)
-                notifyItemRemoved(position)
-                notifyItemRangeChanged(position,manzaralar.size)
-            }
         }
 
     }
+    fun removeAt(position: Int) {
+        manzaralar.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+
+
 }
